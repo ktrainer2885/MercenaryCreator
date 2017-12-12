498f16db1edbe517ef347a8466f481fb974da092
Rogelio Sergio Ramirez III <rogelio.ramirez@gmail.com>
2017-12-12 16:07:19 -0600

    Added edit page, changed some routes (edit page nonfunctional still)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,20 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_user
  
  # @user = current_user
  # @units = @user.units
+
+ def get_user
+  @user = current_user
+ end
     
   def index
-      @user = current_user
-      @units = @user.units
+  
+    @units = @user.units
+  end
+
+  def edit
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   get "/admin/index", to: "admin#index"
+  get "/dashboard/", to: "dashboard#index"
   get '/dashboard/index', to: "dashboard#index"
+  get '/dashboard/edit', to: "dashboard#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
